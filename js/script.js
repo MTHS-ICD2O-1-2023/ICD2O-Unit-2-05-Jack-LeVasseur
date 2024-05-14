132b3c66aa0f358a88d51dd9e5f8d9db8ba379d9
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-05-14 21:10:52 +0000

    Updated index.html

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict";
+
+function calculatePayAndTaxes() {
+    // input
+    const numberOfHoursYouWorked = parseFloat(document.getElementById('number-of-hours-you-worked').value);
+    const timesYourSalary = parseFloat(document.getElementById('times-your-salary').value);
+    const taxRate = 0.18;
+
+    // process
+    const pay = (numberOfHoursYouWorked * timesYourSalary) * (1 - taxRate);
+    const taxes = (numberOfHoursYouWorked * timesYourSalary) * taxRate;
+
+    // output
+    document.getElementById('pay').innerHTML = 'Your pay will be: $' + pay.toFixed(2);
+    document.getElementById('taxes').innerHTML = 'The government will take: $' + taxes.toFixed(2);
+}
